79adce70c8197c51cc1e45c482f18b016bb388c3
Caleb Morin <cmorin2@my.centennialcollege.ca>
2019-11-09 09:46:51 -0500

    First Commit

---
+++ Assets/_Scripts/Speed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///Project Information
+/// 
+/// Original Author: Tom Tsiliopolous
+/// Date:
+/// 
+///Project Information
+
+namespace Util
+{
+    [System.Serializable]
+    public class Speed
+    {
+        public float min;
+        public float max;
+    }
+}
+
